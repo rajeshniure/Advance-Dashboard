4cf205af16cca95c5abe9905c9b776b883c49429
rajesh <rajeshniure567@gmail.com>
2025-10-18 16:47:50 +0545

    build fix 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: 'react-chartjs-2', replacement: `${process.cwd()}/node_modules/react-chartjs-2/dist/index.js` },
-  { find: 'cookie', replacement: `${process.cwd()}/node_modules/cookie/dist/index.js` },
-    ],
-  },
-  optimizeDeps: {
-    include: ['react-chartjs-2', 'chart.js', 'cookie', '@mui/icons-material'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/, /node_modules\/cookie/, /node_modules\/@mui\//],
-    },
-    rollupOptions: {
-      external: ['@mui/icons-material'],
-    },
-  },
 })
